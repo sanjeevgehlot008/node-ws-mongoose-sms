efde928eb56a9dd3033841f29f2bf773513d72f2
sanjeev gehlot <sanjeev.gehlot@mindcrewtech.com>
2018-02-14 19:48:26 +0530

    changing connection db

--- web-site/model/connection.js
+++ web-site/model/connection.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://192.168.99.100/nodewebappdbwebsocket');
+mongoose.connect('mongodb://userNPB:7KWwgoSkwuKam5II@mongodb/nodewebappdbwebsocket');
 
 var connectionOne = mongoose.connection;
 module.exports = connectionOne;
